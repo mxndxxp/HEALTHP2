59b1ac0582e406e51423d4ddffc2ca869ecca48b
Mandeep Ghosh <mandyxxxghoshxxx@gmail.com>
2025-07-17 11:44:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,6 +6,8 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// These variables are now being read from .env.local
+// NEXT_PUBLIC_ is for client-side access, the server-side can access them directly
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
